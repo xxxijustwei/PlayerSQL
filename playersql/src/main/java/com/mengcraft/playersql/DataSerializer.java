648d9dcd74e7f21f1efe77ee6f91bc9148577bfb
justwei <admin@taylorswiftcn.com>
2021-12-09 22:01:05 +0800

    [update] Add itemstack amount after zaphkiel serializer

--- playersql/src/main/java/com/mengcraft/playersql/DataSerializer.java
+++ playersql/src/main/java/com/mengcraft/playersql/DataSerializer.java
@@ -1,5 +1,7 @@
 package com.mengcraft.playersql;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import ink.ptms.zaphkiel.ZaphkielAPI;
 import ink.ptms.zaphkiel.api.ItemStream;
 import lombok.SneakyThrows;
@@ -9,19 +11,28 @@ import org.bukkit.inventory.ItemStack;
 
 public class DataSerializer {
 
+    private final static Gson gson = new Gson();
+
     @SneakyThrows
     public static String serialize(ItemStack input) {
         ItemStream itemStream = ZaphkielAPI.INSTANCE.read(input);
         if (itemStream.isVanilla()) return "";
 
-        return ZaphkielAPI.INSTANCE.serialize(input).toString();
+        JsonObject result = ZaphkielAPI.INSTANCE.serialize(input);
+        result.addProperty("amount", input.getAmount());
+
+        return result.toString();
     }
 
     @SneakyThrows
     public static ItemStack deserialize(Player player, String input) {
         try {
-            ItemStream itemStream = ZaphkielAPI.INSTANCE.deserialize(input);
-            return itemStream.rebuildToItemStack(player);
+            JsonObject target = gson.fromJson(input, JsonObject.class);
+            ItemStream itemStream = ZaphkielAPI.INSTANCE.deserialize(target);
+            ItemStack itemStack = itemStream.rebuildToItemStack(player);
+            itemStack.setAmount(target.get("amount").getAsInt());
+
+            return itemStack;
         }
         catch (Exception e) {
             return new ItemStack(Material.AIR);
